7c369afcccac3827be15cc75638d93bfeffaac3d
parsariyahi <pany.parsariyahi@gmail.com>
2023-06-24 16:58:42 +0330

    Add ``delivered`` endpoint, driver checker for carries

--- api/carry/urls.py
+++ api/carry/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from .views import (
-    add_carry_manual, add_carry_auto, get_carry, list_carries,
+    add_carry_manual, add_carry_auto, 
+    get_carry, list_carries, delivered_carry
 )
 
 urlpatterns = [
     path("add/", add_carry_manual),
     path("add/auto", add_carry_auto),
     path("<int:pk>/", get_carry),
+    path("<int:pk>/delivered", delivered_carry),
     path("all/", list_carries),
 ]
--- api/carry/views.py
+++ api/carry/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,7 +9,7 @@ from .serializers import CarrySerializer
 
 from user.models import CustomUser
 from driver.models import Driver
-from api.globals import token_finder, authenticate, get_user_obj, is_manager
+from api.globals import token_finder, authenticate, get_user_obj, is_manager, is_driver
 
 
 @api_view(["POST"])
@@ -78,8 +80,19 @@ def add_carry_manual(request):
 
     serializer = CarrySerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
+
+        # check if the driver is free.
+        driver_id = request.data.get("driver", None)
+        driver = Driver.objects.get(pk=driver_id)
+        if driver.is_free:
+            driver.is_free = False
+            driver.save()
+            serializer.save()
+
+            return Response(status=status.HTTP_201_CREATED)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(["GET"])
 def get_carry(request, pk):
@@ -119,3 +132,27 @@ def list_carries(request):
     carries = Carry.objects.all()
     serializer = CarrySerializer(carries, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["GET"])
+def delivered_carry(request, pk):
+    username = request.headers.get("UserName")
+    user = get_user_obj(username=username)
+    if not user:
+        return Response("User Not Found", status=status.HTTP_401_UNAUTHORIZED)
+
+    if not is_driver(user):
+        return Response("User is Not Driver", status=status.HTTP_401_UNAUTHORIZED)
+
+    auth_token = request.headers.get("Authorization")
+    token = token_finder(auth_token)
+    if not authenticate(user, token):
+        return Response("Invalid Token", status=status.HTTP_401_UNAUTHORIZED)
+
+    carry = Carry.objects.get(pk=pk)
+    carry.delivered = True
+    carry.delivered_at = datetime.now()
+    carry.driver.is_free = True
+    carry.driver.save()
+    carry.save()
+
+    return Response(status=status.HTTP_200_OK)
