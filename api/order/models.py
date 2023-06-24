20adfe7fee65ed22cc6531c7746588ed42a48b40
parsariyahi <pany.parsariyahi@gmail.com>
2023-06-24 17:30:09 +0330

    Add ``src``,``dest`` latitude and longitude

--- api/order/models.py
+++ api/order/models.py
@@ -4,7 +4,9 @@ from manager.models import Manager
 class Order(models.Model):
     manager = models.ForeignKey(Manager, on_delete=models.RESTRICT)
     title = models.CharField(max_length=250)
-    source = models.CharField(max_length=250)
-    destination = models.CharField(max_length=250)
+    src_lat = models.IntegerField()
+    src_long = models.IntegerField()
+    dest_lat = models.IntegerField()
+    dest_long = models.IntegerField()
     status = models.IntegerField()
     created_at = models.DateTimeField(auto_now=True)
